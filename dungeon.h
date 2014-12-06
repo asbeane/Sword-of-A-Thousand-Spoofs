7e3e25d8d43265cdfc0db9b48c8440bd8883ea89
SemanticPD <asbeane@gmail.com>
2014-12-06 14:26:00 -0500

    Started dungeon.h

---
+++ dungeon.h
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include"room.h"
+
+using namespace std;
+
+/*New Implementation of Dungeon Generator with Class Implementation*/
+
+class Dungeon
+{
+    protected:
+	     vector<Room> dungeon;
+		  char ascii_map[6][5];
+
+    public:
+		  Dungeon();
+
+		  
+
+};
